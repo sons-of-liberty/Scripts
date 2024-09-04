83ffe52ae220afe93068ce0b84b09765de0d71e9
sons-of-liberty <138848803+sons-of-liberty@users.noreply.github.com>
2024-09-04 12:43:03 +0330

    Update Warp.sh

--- Warp.sh
+++ Warp.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 read -p "Enter Warp Plus Key: " warp_key
+read -p "Enter Endpoint: " endpoint
+line_to_add="Table = off"
 
 wget https://github.com/ViRb3/wgcf/releases/download/v2.2.22/wgcf_2.2.22_linux_amd64
 mv wgcf_2.2.22_linux_amd64 /usr/bin/wgcf
@@ -13,3 +15,8 @@ wgcf update
 wgcf generate
 
 apt install -y wireguard; apt install -y wireguard-dkms; apt install -y wireguard-tools; apt install -y resolvconf
+
+sed -i "/\[Peer\]/i $line_to_add" /usr/bin/wgcf-profile.conf
+sed -i "s/^Endpoint.*/Endpoint = '$endpoint'/" usr/bin/wgcf-profile.conf
+
+mv /usr/bin/wgcf-profile.conf /etc/wireguard/warp.conf
