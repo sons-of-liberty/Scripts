e74555f09f494fd6b2d958df648bed92acef6e2a
sons-of-liberty <138848803+sons-of-liberty@users.noreply.github.com>
2024-09-04 12:24:30 +0330

    Update Warp.sh

--- Warp.sh
+++ Warp.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+read -p "Enter Warp Plus Key: " warp_key
+
 wget https://github.com/ViRb3/wgcf/releases/download/v2.2.22/wgcf_2.2.22_linux_amd64
+mv wgcf_2.2.22_linux_amd64 /usr/bin/wgcf
+chmod +x /usr/bin/wgcf
+wgcf register
+wgcf generate
+
+sed -i "s/^license_key.*/license_key = '$warp_key'/" /usr/bin/wgcf-account.toml
+
+wgcf update
+wgcf generate
+
