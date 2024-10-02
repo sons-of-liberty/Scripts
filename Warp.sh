2eb62d5802af3a35a105348dd718fddc4720bbf3
sons-of-liberty <138848803+sons-of-liberty@users.noreply.github.com>
2024-10-03 02:32:02 +0330

    Update Warp.sh

--- Warp.sh
+++ Warp.sh
@@ -1,27 +1,15 @@
 #!/bin/bash
-read -p "Enter Warp Plus Key: " warp_key
-read -p "Enter Endpoint: " endpoint
+
 
 wget https://github.com/ViRb3/wgcf/releases/download/v2.2.22/wgcf_2.2.22_linux_amd64
 mv wgcf_2.2.22_linux_amd64 /usr/bin/wgcf
 chmod +x /usr/bin/wgcf
 cd /usr/bin/
 wgcf register
-
-
-sed -i "s/^license_key.*/license_key = '$warp_key'/" /usr/bin/wgcf-account.toml
-
-wgcf update
 wgcf generate
 
 apt install -y wireguard; apt install -y wireguard-dkms; apt install -y wireguard-tools; apt install -y resolvconf
-
-sed -i '/:/d' /usr/bin/wgcf-profile.conf
 sed -i '/\[Peer\]/i\
 Table = off' /usr/bin/wgcf-profile.conf
-sed -i '/\[Peer\]/i\
-DNS = 1.1.1.1, 1.0.0.1' /usr/bin/wgcf-profile.conf
-echo "Endpoint = $endpoint" >> /usr/bin/wgcf-profile.conf
-
 mv /usr/bin/wgcf-profile.conf /etc/wireguard/warp.conf
 systemctl enable --now wg-quick@warp
